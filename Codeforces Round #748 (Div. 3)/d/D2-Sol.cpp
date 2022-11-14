42af266b30ba33a92694f1ce9c9817300e3c26a3
Omar Salah <63765054+Omar-hk@users.noreply.github.com>
2022-11-14 20:57:29 +0200

    Update D2-Sol.cpp

--- Codeforces Round #748 (Div. 3)/d/D2-Sol.cpp
+++ Codeforces Round #748 (Div. 3)/d/D2-Sol.cpp
@@ -2,7 +2,6 @@
 
 
 using namespace std;
-#define Ios_Base ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
 #define ff               first
 #define ss               second
 #define pb               push_back
@@ -15,27 +14,106 @@ using namespace std;
 #define dbgA(X)          for(auto i: X) cout << i << " "; cel
 #define dbg(x)          cout << x; cel
 #define endl '\n'
+#define ll long long
 
-bool solve()
+int main()
 {
-    int n;
-    cin>>n;
-    vector<int> a(n);
-    for(int i =0; i < n; i++)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t, n, c, i, j, ans, k, d, l, p, p1, c1;
+
+    cin>>t;
+
+    for(;t--;)
     {
-        
+        cin>>n;
+
+        ll a[n];
+
+        for(i=0; i<n; i++)
+        {
+            cin>>a[i];
+        }
+
+        sort(a, a+n);
+
+        ans=0;
+
+        for(i=0; i<n;)
+        {
+            c=0;
+            j=i;
+
+            while(i<n && a[i]==a[j])
+            {
+                c++;
+                i++;
+            }
+            if(c>=(n/2)){
+                ans=-1;
+                break;
+            }
+        }
+
+        if(ans==-1){
+            cout<<ans<<"\n";
+            continue;
+        }
+
+        for(i=0; i<n; i++)
+        {
+            c=1;
+            for(j=i+1; j<n; j++)
+            {
+
+                if(a[j]==a[i]){
+                    c++;
+                    continue;
+                }
+                k=a[j]-a[i];
+
+                for(p=1; p*p<=k; p++)
+                {
+                    if(p>1){
+                        c1=c+1;
+                        if((k%p)==0){
+
+                            for(l=j+1; l<n; l++)
+                            {
+                                if(((a[l]-a[i])%p)==0)
+                                {
+                                    c1++;
+                                }
+                            }
+                            if(c1>=(n/2)){
+                                ans=max(ans, p);
+                            }
+                        }
+                    }
+
+                    p1=k/p;
+
+                    if(p1!=p){
+                        if((k%p1)==0){
+                            c1=c+1;
+                            for(l=j+1; l<n; l++)
+                            {
+                                if(((a[l]-a[i])%p1)==0)
+                                {
+                                    c1++;
+                                }
+                            }
+                            if(c1>=(n/2)){
+                                ans=max(ans, p1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        cout<<ans<<"\n";
     }
-}
-
-
-
-int32_t main()
-{
-    int tt;
-    cin >> tt;
-    while (tt--)
-    {
-        solve();
-    }
-    
 }
